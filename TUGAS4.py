c2e85076442f0cc4e842d4d8fc6c9c138ef74378
Denis656344 <deka27361@gmail.com>
2024-10-28 00:07:35 +0800

    Add files via upload

---
+++ TUGAS4.py
@@ -0,0 +1,10 @@
+nama= "denish eka putra gunanda"
+alamat= "KLU"
+NIM= "24241172"
+nimpertama= 24
+nimakhir= 72
+hasil= nimpertama + nimakhir
+
+print("nama saya adalah "+ nama)
+print("saya berasal dari "+ alamat)
+print(f"hasil penjumlahan dari {nimpertama} + {nimakhir} = {hasil}")
