4a1b3cf298df7ec9ec64e01cba841fc59cc8052c
Denis656344 <deka27361@gmail.com>
2024-11-08 10:02:56 +0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,16 @@
+# variable assigment value
+x = 9
+y = 6
+print("nilai dari x adalah : ", y)
+
+# aturan penamaan variable
+luas = 30 # bisa berupa kata
+denis_ = 100 # jika dua kata, digabung dengan "_"
+KeziaX = # boleh gabung capital dan non capital
+KeziaX = 3
+lenca88 = 10 # jika menggabungkan huruf dan angka, angka di akhir 
+print("nilai dari kezia adalah : ", lenca88)
+
+
+a = 4 # integer bilangan bulat 
+print("tipe data dari a : ", type(d))
